84c6a687b607647668cfc2ca858401fa0172eca3
Hiep Nguyen <hoanghiep2004hy@gmail.com>
2021-01-01 10:27:17 +0700

    Updates

--- src/events/order-cancelled-event.ts
+++ src/events/order-cancelled-event.ts
@@ -1,11 +1,11 @@
 import { Subjects } from './subjects';
 
 export interface OrderCancelledEvent {
-    subject: Subjects.OrderCancelled;
-    data: {
-        id: string;
-        ticket: {
-            id: string;
-        }
-    }
+  subject: Subjects.OrderCancelled;
+  data: {
+    id: string;
+    ticket: {
+      id: string;
+    };
+  };
 }

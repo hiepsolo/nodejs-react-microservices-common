6c6d9bf976f0d6ef21b1e3fb3c12454516760d80
Hiep Nguyen <hoanghiep2004hy@gmail.com>
2021-01-01 10:10:10 +0700

    Updates

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,19 @@
-interface Color {
-    red: number;
-    blue: number;
-    green: number;
-}
+// Re-export stuff from errors and middlewares
+export * from './errors/bad-request-error';
+export * from './errors/custom-error';
+export * from './errors/database-connection-error';
+export * from './errors/not-authorized-error';
+export * from './errors/not-found-error';
+export * from './errors/request-validation-error';
 
-const color: Color = {
-    red: 10,
-    blue: 10,
-    green: 10
-}
+export * from './middlewares/current-user';
+export * from './middlewares/error-handler';
+export * from './middlewares/require-auth';
+export * from './middlewares/validate-request';
+
+export * from './events/base-listener';
+export * from './events/base-publisher';
+export * from './events/subjects';
+export * from './events/ticket-created-event';
+export * from './events/ticket-updated-event';
+export * from './events/types/order-status';
